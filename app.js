e7b0af93b2d4187477fbb7be7daa824fbbcc011b
NicogR72 <nicolasgarciar22@gmail.com>
2021-10-09 16:50:59 -0500

    Agrego servidor express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+  let html = path.join(__dirname, "./views/index.html");
+  res.sendFile(html);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor Corriendo");
+});
