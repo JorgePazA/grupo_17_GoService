50bab4878d7e8a4a3e07d90fed3cdc3773738917
juannc90 <juanc.ladinog@gmail.com>
2021-10-21 23:43:47 -0500

    Edición de la barra de buscar y se incluye el archivo sin estructura productCar.html

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.get("/", (req, res) => {
   let html = path.join(__dirname, "./views/index.html");
   res.sendFile(html);
 });
+app.get("/productCar.html", (req, res) => {
+  res.sendFile(path.resolve(__dirname,"./views/productCar.html"));
+});
 
 app.listen(3000, () => {
   console.log("Servidor Corriendo");
