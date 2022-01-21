4f32cd02d4b459dd0d710646d48633421c35d1d8
juannc90 <juanc.ladinog@gmail.com>
2022-01-21 09:58:24 -0500

    Se realizan los modelos para el CRUD de users y gogers

--- src/model/gogerModel.js
+++ src/model/gogerModel.js
@@ -1,7 +1,7 @@
 const db = require('../database/models')
 
 const gogerModel = {
-    getAll: async() => {
+    getAll: async () => {
         try {
             const result = await db.gogers.findAll()
             console.log(result)
@@ -9,7 +9,7 @@ const gogerModel = {
             console.log(`Ocurrió un error ${error.message}`)
         }
     },
-    getOne: async(id) => {
+    getOne: async (id) => {
         try {
             const result = await db.gogers.findByPk(id)
             console.log(result)
@@ -17,12 +17,57 @@ const gogerModel = {
             console.log(`Ocurrió un error ${error.message}`)
         }
     },
-    create: async(goger) => {
+    create: async (gogerData) => {
         try {
-            const result = await db.gogers.create(goger)
+            const result = await db.gogers.create(gogerData)
             console.log(result)
         } catch (error) {
             console.log(`Ocurrió un error ${error.message}`)
         }
+    },
+    update: async (gogerData, id) => {
+        try {
+            const result = await db.gogers
+                .update(
+                    {
+                        fullName: gogerData.fullName,
+                        description: gogerData.description,
+                        price: gogerData.price,
+                        image: gogerData.image,
+                        experience: gogerData.experience,
+                        categories_id: gogerData.categories_id
+                    },
+                    {
+                        where: { id: id }
+                    })
+            console.log(result)
+        } catch (error) {
+            console.log(`Ocurrió un error ${error.message}`)
+        }
+    },
+    destroy: async (id) => {
+        try {
+            const result = await db.gogers
+                .destroy(
+                    {
+                        where: { id: id }
+                    })
+                    console.log(result)
+        } catch (error) {
+            console.log(`Ocurrió un error ${error.message}`)
+        }
+            // .destroy({ where: { id: userId }, force: true }) // force: true es para asegurar que se ejecute la acción     
     }
 }
+
+// gogerModel.create({
+//     fullName: 'Prueba Gogers',
+//     description: 'Esta es una prueba para crear un goger',
+//     price: 30000,
+//     image: 'eawlñkkdaslekñl',
+//     experience: 3,
+//     categories_id: 1
+// })
+// gogerModel.getAll()
+
+module.exports = gogerModel
--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,7 +1,7 @@
 const db = require('../database/models')
 
 const userModel = {
-    getAll: async() => {
+    getAll: async () => {
         try {
             const result = await db.users.findAll()
             console.log(result)
@@ -9,7 +9,7 @@ const userModel = {
             console.log(`Ocurrió un error ${error.message}`)
         }
     },
-    getOne: async(id) => {
+    getOne: async (id) => {
         try {
             const result = await db.users.findByPk(id)
             console.log(result)
@@ -17,7 +17,7 @@ const userModel = {
             console.log(`Ocurrió un error ${error.message}`)
         }
     },
-    create: async(userData, fileName) => {
+    create: async (userData) => {
         try {
             const result = await db.users.create(userData)
             console.log(result)
@@ -25,50 +25,49 @@ const userModel = {
             console.log(`Ocurrió un error ${error.message}`)
         }
     },
-    //Revisar como cambiar el then por async await
     update: async (userData, id) => {
         try {
             const result = await db.users
-            .update(
-            {
-                name: userData.name,
-                lastName: userData.lastName,
-                email: userData.email,
-                password: userData.password,
-                avatar: userData.avatar,
-                rol: userData.rol
-            
-            },
-            {
-                where: {id: id}
-            })
+                .update(
+                    {
+                        name: userData.name,
+                        lastName: userData.lastName,
+                        email: userData.email,
+                        password: userData.password,
+                        avatar: userData.avatar,
+                        rol: userData.rol
+                    },
+                    {
+                        where: { id: id }
+                    })
             console.log(result)
         } catch (error) {
             console.log(`Ocurrió un error ${error.message}`)
         }
     },
-    //Revisar como cambiar el then por async await
-    destroy: function (req,res) {
-        let userId = req.params.id;
-        users
-        .destroy({where: {id: userId}, force: true}) // force: true es para asegurar que se ejecute la acción
-        .then(()=>{
-            return res.redirect('/movies')})
-        .catch(error => res.send(error)) 
+    destroy: async (id) => {
+        try {
+            const result = await db.users
+                .destroy(
+                    {
+                        where: { id: id }
+                    })
+                    console.log(result)
+        } catch (error) {
+            console.log(`Ocurrió un error ${error.message}`)
+        }
+            // .destroy({ where: { id: userId }, force: true }) // force: true es para asegurar que se ejecute la acción     
     }
 }
 
-userModel.update({
-    name: 'Fran',
-    lastName: 'Paz',
-    email: 'jorge@gmail.com',
-    password: '1234',
-    avatar: 'dsadasdasdaswwq',
-    rol: 1
-},
-2
-
-
-)
+// userModel.create({
+//     name: 'Prueba',
+//     lastName: 'Users',
+//     email: 'prueba@gmail.com',
+//     password: '1234',
+//     avatar: 'dsadasdasdaswwq',
+//     rol: 1
+// })
+// userModel.getAll()
 
 module.exports = userModel
