36ae1e9dd8f90d754e8ea3562640b7675852c1d5
JorgePazA <jorgepaz1993@gmail.com>
2022-01-27 16:29:31 -0500

    Validando varioables de entorno en produccion

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,17 +8,17 @@ module.exports = {
     "port": "3306"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DBUSER,
+    "password": process.env.DBPASS,
+    "database": process.env.DBNAME,
+    "host": process.env.DBHOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DBUSER,
+    "password": process.env.DBPASS,
+    "database": process.env.DBNAME,
+    "host": process.env.DBHOST,
     "dialect": "mysql"
   }
 }
