c5fa639e65e5632c9234b48d222c9f41003a98a1
juannc90 <juanc.ladinog@gmail.com>
2022-02-14 20:32:09 -0500

    Se agregan validaciones de la longitud de los campos del formulario de nuevo Goger

--- src/middlewares/validateRegisterGogerMiddleware.js
+++ src/middlewares/validateRegisterGogerMiddleware.js
@@ -9,8 +9,10 @@ const { body } = require('express-validator')
 
 const validationsGoger = [
     body("categories_id").notEmpty().withMessage("Tienes que elegir una categoría"),
-    body("fullName").notEmpty().withMessage("Tienes que escribir un nombre"),
-    body("description").notEmpty().withMessage("Tienes que agregar una descripción"),
+    body("fullName").notEmpty().withMessage("Tienes que escribir un nombre").bail()
+        .isLength(5).withMessage("El nombre de tener al menos 5 caracteres"),
+    body("description").notEmpty().withMessage("Tienes que agregar una descripción").bail()
+        .isLength(20).withMessage("La descripción debe contener al menos 20 caracteres"),
     body("price").notEmpty().withMessage("Tienes que agregar el precio por los servicios del Goger"),
     body("experience").notEmpty().withMessage("Tienes que especificar la experiencia del Goger"),
     body("product_image").custom((value, { req }) => {
